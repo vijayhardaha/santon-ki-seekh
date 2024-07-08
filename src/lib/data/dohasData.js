0cab0b261bc437626940b68a1fed4dcd78ce51d2
Vijay Hardaha <vijayhardaha@gmail.com>
2024-07-09 03:16:41 +0530

    feat(add): implement api for data

--- src/lib/data/dohasData.js
+++ src/lib/data/dohasData.js
@@ -1,260 +1,41 @@
-import { SANT_KABIR } from "../helpers/constants.js";
+import axios from "axios";
+import {SANT_KABIR} from "../helpers/constants.js";
 
-const dataSet = [
-  {
-    id: "aag-aanch-sehna-sugam",
-    author: SANT_KABIR,
-    content: [
-      "आग आंच सहना सुगम, सुगम खड़ग की धार ।",
-      "नेह निबाहन एक रस, महा-कठिन व्यवहार ।।",
-    ],
-  },
-  {
-    id: "aapa-mete-hari-mile",
-    author: SANT_KABIR,
-    content: [
-      "आपा मेटे हरि मिले, हरि मेटे सब जाई ।",
-      "अकथ कहानी प्रेम की, कोई नहीं पतियाय ।।",
-    ],
-  },
-  {
-    id: "kabira-yeh-sansar-hai",
-    author: SANT_KABIR,
-    content: [
-      "कबीरा यह संसार है, जैसा सेमल फूल ।",
-      "दिन दस के व्यवहार में, झूठे रंग ना भूल ।।",
-    ],
-  },
-  {
-    id: "kabira-rekha-sindoor-ki",
-    author: SANT_KABIR,
-    content: [
-      "कबीरा रेख सिंदूर की, काजर किया न जाय ।",
-      "तन मन में प्रीतम बसा, दूजा कहाँ समय ।।",
-    ],
-  },
-  {
-    id: "gagan-damama-bajiya",
-    author: SANT_KABIR,
-    content: [
-      "गगन दमामा बाजिया, पड़े निसाने घाव ।",
-      "खेत बुहारे सूरमा, मोहे मरण का चाव ।।",
-    ],
-  },
-  {
-    id: "jal-mein-base-kumudani",
-    author: SANT_KABIR,
-    content: [
-      "जल में बसे कुमुदनी, चंदा बसे अकास ।",
-      "जैसी जिसकी भावना, सो ताही के पास ।।",
-    ],
-  },
-  {
-    id: "jahan-kam-tahan-naam-naheen",
-    author: SANT_KABIR,
-    content: [
-      "जहाँ काम तहाँ नाम नहीं, जहाँ नाम नहि काम ।",
-      "दोनो कबहू ना मिलै, रवि रजनी एक ठाम ।।",
-    ],
-  },
-  {
-    id: "jehi-ghat-preeti-na-prem-ras",
-    author: SANT_KABIR,
-    content: [
-      "जिहि घट प्रीति न प्रेम रस, पुनि रसना नहीं राम ।",
-      "ते नर इस संसार में, उपजि भए बेकाम ।।",
-    ],
-  },
-  {
-    id: "gyani-bhule-gyan-kath",
-    author: SANT_KABIR,
-    content: [
-      "ज्ञानी भूले ज्ञान कथि, निकट रहा निज रूप ।",
-      "बाहिर खोजै बापुरै, भीतर वस्तु अनूप ।।",
-    ],
-  },
-  {
-    id: "durlabh-manush-janm-hai",
-    author: SANT_KABIR,
-    content: [
-      "दुर्लभ मानुष जन्म है, देह न बारम्बार ।",
-      "तरुवर ज्यों पत्ता झड़े, बहुरि न लागे डार ।।",
-    ],
-  },
-  {
-    id: "daudat-daudat-daudia",
-    author: SANT_KABIR,
-    content: [
-      "दौड़त दौड़त दौड़िया, जेती मन की दौड़ ।",
-      "दौड़ि थके मन थिर भया, वस्तु ठौर की ठौर ।।",
-    ],
-  },
-  {
-    id: "preeti-bahut-sansar-mein",
-    author: SANT_KABIR,
-    content: [
-      "प्रीति बहुत संसार में, नाना विधि की सोय ।",
-      "उत्तम प्रीति सो जानिये, सतगुरु से जो होय ।।",
-    ],
-  },
-  {
-    id: "prem-piyala-jo-piye",
-    author: SANT_KABIR,
-    content: [
-      "प्रेम पियाला जो पिये, शीश दच्छिना देय ।",
-      "लोभी शीश न दे सके, नाम प्रेम का लेय ।।",
-    ],
-  },
-  {
-    id: "bahata-pani-nirmala",
-    author: SANT_KABIR,
-    content: [
-      "बहता पानी निर्मला, बंधे सो गंदा होए ।",
-      "साधु जन रमता भला, दाग न लागे कोये ।।",
-    ],
-  },
-  {
-    id: "bura-jo-dekhan-main-chala",
-    author: SANT_KABIR,
-    content: [
-      "बुरा जो देखन मैं चला, बुरा न मिलिया कोए ।",
-      "जो मन खोजा आपना, मुझसे बुरा न कोए ।।",
-    ],
-  },
-  {
-    id: "bheetar-to-bheda-naheen",
-    author: SANT_KABIR,
-    content: [
-      "भीतर तो भेदा नहीं, बाहिर कथै अनेक ।",
-      "जो पाई भीतर लखि परै,भीतर बाहर एक ।।",
-    ],
-  },
-  {
-    id: "manuva-to-panchi-bhaya",
-    author: SANT_KABIR,
-    content: [
-      "मनुवा तो पंछी भया, उड़ि के चला अकास ।",
-      "ऊपर ही ते गिर पड़ा, मन माया के पास ।।",
-    ],
-  },
-  {
-    id: "maran-bhala-tab-janiye",
-    author: SANT_KABIR,
-    content: [
-      "मरण भला तब जानिए, छूट जाए हंकार ।",
-      "जग की मरनी क्यों मरें, दिन में सौसौ बार ।।",
-    ],
-  },
-  {
-    id: "maya-chhodan-sab-kahe",
-    author: SANT_KABIR,
-    content: [
-      "माया छोड़न सब कहे, माया छोड़ी न जाय ।",
-      "छोड़न की जो बात करूं, बहुत तमाचा खाय ।।",
-    ],
-  },
-  {
-    id: "maya-to-thagni-bhai",
-    author: SANT_KABIR,
-    content: [
-      "माया तो ठगनी भई, ठगत फिरै सब देस ।",
-      "जा ठग ने ठगनी ठगी, ता ठग को आदेश ।।",
-    ],
-  },
-  {
-    id: "maya-doy-prakar-ki",
-    author: SANT_KABIR,
-    content: [
-      "माया दोय प्रकार की, जो कोय जाने खाय ।",
-      "एक मिलावे राम से, एक नरक ले जाय ।।",
-    ],
-  },
-  {
-    id: "maya-man-ki-mohini",
-    author: SANT_KABIR,
-    content: [
-      "माया मन की मोहिनी, सुर नर रहे 'लुभाय ।",
-      "इन माया सब खाइया, माया कोय न खाय ॥",
-    ],
-  },
-  {
-    id: "moti-maya-sab-tajai",
-    author: SANT_KABIR,
-    content: [
-      "मोटी माया सब तजै, झीनी तजी न जाय ।",
-      "पीर पैगम्बर औलिया, झीनी सबको खाय ।।",
-    ],
-  },
-  {
-    id: "ram-naam-kadva-lage",
-    author: SANT_KABIR,
-    content: [
-      "राम नाम कड़वा लगे, मीठा लागे दाम ।",
-      "दुविधा में दोनों गए माया मिली ना राम ।।",
-    ],
-  },
-  {
-    id: "ram-bulava-bhejiya",
-    author: SANT_KABIR,
-    content: [
-      "राम बुलावा भेजिया, दिया कबीरा रोय ।",
-      "जो सुख साधू संग में, सो बैकुंठ ना होय ।।",
-    ],
-  },
-  {
-    id: "sadhu-aisa-chahiye",
-    author: SANT_KABIR,
-    content: [
-      "साधु ऐसा चाहिए, जैसा सूप सुभाय ।",
-      "सार-सार को गहि रहै, थोथा देई उड़ाए ।।",
-    ],
-  },
-  {
-    id: "sadho-ran-mein-mit-raha",
-    author: SANT_KABIR,
-    content: [
-      "साधो रण में मिट रहा, टूटन दे हंकार ।",
-      "जग की मरनी क्यों मरें, दिन में सौसौ बार ।।",
-    ],
-  },
-  {
-    id: "sukh-ke-sangi-swarathi",
-    author: SANT_KABIR,
-    content: [
-      "सुख के संगी स्वारथी, दुःख में रहते दूर ।",
-      "कहैं कबीर परमारथी, दुःख सुख सदा हजूर ।।",
-    ],
-  },
-  {
-    id: "surati-karo-mere-saiya",
-    author: SANT_KABIR,
-    content: [
-      "सुरति करो मेरे सांइया, हम है भवजल मांहि ।",
-      "आपे ही बहि जाएंगे, जो नहीं पकड़ो बांहि ।।",
-    ],
-  },
-  {
-    id: "soora-soi-saraihiye",
-    author: SANT_KABIR,
-    content: [
-      "सूरा सोई सराहिये, लड़े मुक्ति के हेत ।",
-      "पुर्जा पुर्जा कट पड़े, तो भी ना छाड़े खेत ।।",
-    ],
-  },
-  {
-    id: "sona-sajjan-sadhu-jan",
-    author: SANT_KABIR,
-    content: [
-      "सोना सज्जन साधुजन, टूट जुड़े सौ बार ।",
-      "दुर्जन कुंभ कुम्हार के, एके धक्का दरार ।।",
-    ],
-  },
-];
+// URL to fetch Kabir Ke Dohe API data
+const apiUrl = "https://santo-ki-seekh.netlify.app/api/kabir-ke-dohe/";
 
-const DohaMeta = {
-  fileName: "santon-ke-dohe",
-  mdTitle: "संतों के दोहे (Couplets)",
-  data: dataSet,
-};
+/**
+ * Fetches Kabir Ke Dohe data from the specified API endpoint.
+ * @return {Promise<Object>} A promise that resolves to an object containing metadata and filtered dohe data.
+ * @throws {Error} If there's an error fetching or processing the data.
+ */
+async function fetchDoheData() {
+  try {
+    // Fetch data from API endpoint
+    const response = await axios.get(apiUrl);
+    const doheData = response.data;
 
-export default DohaMeta;
+    // Process or filter data as needed
+    const filteredData = doheData
+      .filter((item) => item.doha && item.doha.trim() !== "") // Filter out empty doha
+      .map((item) => ({
+        id: item.slug, // Assuming item has a slug property for ID
+        content: item.doha.split("\n").map((doha) => doha.trim()),
+        author: SANT_KABIR, // Replace with actual author data if available
+        meaning: item.meaning, // Assuming item has a meaning property
+      }));
+
+    const DohaMeta = {
+      fileName: "santon-ke-dohe",
+      mdTitle: "संतों के दोहे (Couplets)",
+      data: filteredData, // Using filteredData instead of dataSet
+    };
+
+    return DohaMeta;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error; // Optionally rethrow or handle the error as needed
+  }
+}
+
+export default fetchDoheData;
