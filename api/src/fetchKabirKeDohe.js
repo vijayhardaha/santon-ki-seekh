2630392849b9243df8de000a83e96c641687a462
Vijay Hardaha <vijayhardaha@gmail.com>
2024-08-07 04:19:33 +0530

    feat(add): use google apis for data fetching

--- api/src/fetchKabirKeDohe.js
+++ api/src/fetchKabirKeDohe.js
@@ -1,46 +1,111 @@
-const axios = require("axios");
-const csv = require("csvtojson");
-
-// URL to fetch the CSV data
-const url = `https://docs.google.com/spreadsheets/d/e/2PACX-1vQuwtO1d7pHNoKmTEUZjc0I08ZvgwoFgYh3paipOaO0L5UrDI4f7n608C5gc9A-DrgQK9QE5Aa2woyd/pub?gid=0&single=true&output=csv`;
+const latinize = require( "latinize" );
+const slugify = require( "slugify" );
 
 /**
  * Utility function to collapse multiple spaces into a single space and trim whitespace.
  * @param {string} str - The input string to clean.
- * @returns {string} The cleaned string.
+ * @return {string} The cleaned string.
  */
-function cleanString(str) {
-  return str.replace(/[^\S\r\n]+/g, ' ').trim();
-}
+const cleanString = ( str ) => str.replace( /[^\S\r\n]+/g, " " ).trim();
+
+/**
+ * Creates a URL-friendly slug from the given text.
+ *
+ * @param {string} text - The text to be converted into a slug.
+ * @return {string} The generated slug.
+ */
+const createSlug = ( text ) => {
+	return slugify( latinize( text ), {
+		lower: true,
+		replacement: "_",
+		strict: true,
+		trim: true,
+	} );
+};
+
+/**
+ * Converts an array of header names to slugified keys.
+ *
+ * @param {string[]} headers - The array of header names.
+ * @return {Object} An object mapping original header names to their slugified versions.
+ */
+const createSlugifiedKeys = ( headers ) => {
+	return headers.reduce( ( acc, header ) => {
+		const slugifiedHeader = createSlug( header );
+		acc[ header ] = slugifiedHeader;
+		return acc;
+	}, {} );
+};
+
+/**
+ * Maps CSV data to JSON with slugified keys.
+ *
+ * @param {Array<Object>} csvData - The parsed CSV data.
+ * @param {Array<string>} headers - The headers from the CSV.
+ * @return {Array<Object>} - The mapped JSON data.
+ */
+const mapCsvDataToJson = ( csvData, headers ) => {
+	const columnMapping = createSlugifiedKeys( headers );
+
+	return csvData.map( ( row ) => {
+		const newRow = {};
+		for ( const key in row ) {
+			if ( columnMapping[ key ] ) {
+				newRow[ columnMapping[ key ] ] = row[ key ];
+			} else {
+				newRow[ key ] = row[ key ];
+			}
+		}
+		return newRow;
+	} );
+};
 
 /**
  * Fetch Kabir Ke Dohe data from Google Sheets.
- * @returns {Promise<Object[]>} Array of cleaned dohe objects
+ * @return {Promise<Object[]>} Array of cleaned dohe objects
  * @throws {Error} Throws an error if fetching or parsing fails
  */
 async function fetchKabirKeDohe() {
+	const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_API_KEY;
+	const SPREADSHEET_ID = process.env.NEXT_PUBLIC_SPREADSHEET_ID;
+
+	const range = "kabir-ke-dohe!A:Z"; // Adjust range as needed
+	const url = `https://sheets.googleapis.com/v4/spreadsheets/${ SPREADSHEET_ID }/values/${ range }?key=${ API_KEY }`;
+
 	try {
-		// Fetch CSV data from Google Sheets
-		const response = await axios.get(url);
-		const csvData = response.data;
+		console.log( url );
+		const response = await fetch( url );
+		if ( ! response.ok ) {
+			throw new Error( "Network response was not ok" );
+		}
 
-		// Convert CSV to JSON
-		const jsonData = await csv().fromString(csvData);
+		const data = await response.json();
 
-		// Clean and format the data
-		const cleanedData = jsonData.map((item) => ({
-			index: parseInt(item["index"]), // Convert index to integer
-			doha: cleanString(item["doha"]), // Replace double spaces with single spaces and trim whitespace
-			line_hindi: cleanString(item["line_hindi"]), // Replace double spaces with single spaces and trim whitespace
-			line_english: cleanString(item["line_english"]), // Replace double spaces with single spaces and trim whitespace
-			slug: item["slug"].trim(), // Trim whitespace from slug
-			meaning: item["meaning"].trim(), // Trim whitespace from meaning
-		}));
+		// Assuming data.values is an array of rows with header data
+		const [ header, ...rows ] = data.values;
+		const jsonData = rows.map( ( row ) => {
+			const obj = {};
+			header.forEach( ( h, i ) => ( obj[ h ] = row[ i ] ) );
+			return obj;
+		} );
 
-		return cleanedData;
-	} catch (error) {
+		const mappedData = mapCsvDataToJson( jsonData, header );
+
+		const processedData = mappedData.map( ( row ) => ( {
+			index: parseInt( row.index ?? 0 ), // Provide a default value if `index` is undefined or not a number
+			doha: cleanString( row.doha ?? "" ), // Provide a default empty string if `doha` is undefined
+			english_doha: cleanString( row.english_doha ?? "" ), // Provide a default empty string if `english_doha` is undefined
+			title_hindi: cleanString( row.title_hindi ?? "" ), // Provide a default empty string if `title_hindi` is undefined
+			title_english: cleanString( row.title_english ?? "" ), // Provide a default empty string if `title_english` is undefined
+			slug: cleanString( row?.slug?.trim() ?? "" ), // Safely handle `slug` and provide a default empty string if undefined
+			meaning: row?.meaning?.trim() ?? "", // Safely handle `meaning` and provide a default empty string if undefined
+			tags: row?.tags?.trim() ?? "", // Safely handle `tags` and provide a default empty string if undefined
+		} ) );
+
+		return processedData;
+	} catch ( error ) {
 		// Throw an error if fetching or parsing fails
-		throw new Error("Failed to fetch data");
+		throw new Error( error );
 	}
 }
 
