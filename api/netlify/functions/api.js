7635e57457bedc8afe23be824782aadb621bde1a
Vijay Hardaha <vijayhardaha@gmail.com>
2024-08-07 04:22:04 +0530

    refactor: lint & fix with new eslint configs

--- api/netlify/functions/api.js
+++ api/netlify/functions/api.js
@@ -1,6 +1,7 @@
-const express = require("express");
-const serverless = require("serverless-http");
-const { fetchKabirKeDohe } = require("../../src/fetchKabirKeDohe");
+const express = require( "express" );
+const serverless = require( "serverless-http" );
+
+const { fetchKabirKeDohe } = require( "../../src/fetchKabirKeDohe" );
 
 // Create an Express router instance
 const router = express.Router();
@@ -9,30 +10,30 @@ const router = express.Router();
 const app = express();
 
 // Middleware to parse JSON bodies
-app.use(express.json());
+app.use( express.json() );
 
 /**
  * Endpoint to fetch Kabir Ke Dohe.
  * @route GET /.netlify/functions/api/kabir-ke-dohe
- * @returns {Object[]} 200 - Array of Kabir Ke Dohe objects
- * @returns {Error} 500 - Internal server error
+ * @return {Object[]} 200 - Array of Kabir Ke Dohe objects
+ * @return {Error} 500 - Internal server error
  */
-router.get("/kabir-ke-dohe", async (req, res) => {
-  try {
-    const data = await fetchKabirKeDohe();
-    res.json(data);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+router.get( "/kabir-ke-dohe", async ( req, res ) => {
+	try {
+		const data = await fetchKabirKeDohe();
+		res.json( data );
+	} catch ( error ) {
+		res.status( 500 ).json( { error: error.message } );
+	}
+} );
 
 // Catch-all for non-existing endpoints
-router.use((req, res) => {
-  res.status(404).json({ error: "Endpoint not found" });
-});
+router.use( ( req, res ) => {
+	res.status( 404 ).json( { error: "Endpoint not found" } );
+} );
 
 // Use the router for all /api routes
-app.use("/api", router);
+app.use( "/api", router );
 
 // Export the Express app wrapped in serverless-http
-module.exports.handler = serverless(app);
+module.exports.handler = serverless( app );
