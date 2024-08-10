5e02dfa1ec23feafe44173ef139998db9389c817
Vijay Hardaha <vijayhardaha@gmail.com>
2024-08-10 13:06:53 +0530

    fix: fix conflict with new object keys from api

--- src/lib/data/dohasData.js
+++ src/lib/data/dohasData.js
@@ -1,42 +1,45 @@
 import { SANT_KABIR } from "../helpers/constants.js";
+import { padIndex } from "../helpers/dataGenerator.js";
 
 // URL to fetch Kabir Ke Dohe API data
-const apiUrl = "https://santo-ki-seekh.netlify.app/api/kabir-ke-dohe/";
+const API_URL = "https://santo-ki-seekh.netlify.app/api/kabir-ke-dohe/";
 
 /**
  * Fetches Kabir Ke Dohe data from the specified API endpoint.
- * @return {Promise<Object>} A promise that resolves to an object containing metadata and filtered dohe data.
+ * Processes the data to filter out empty dohas and formats it with IDs and content.
+ *
+ * @return {Promise<Object>} A promise that resolves to an object containing metadata and filtered doha data.
  * @throws {Error} If there's an error fetching or processing the data.
  */
 async function fetchDoheData() {
 	try {
 		// Fetch data from API endpoint
-		const response = await fetch( apiUrl );
+		const response = await fetch( API_URL );
 		if ( ! response.ok ) {
 			throw new Error( "Network response was not ok" );
 		}
 
-		const doheData = await response.json();
+		const verses = await response.json();
 
-		// Process or filter data as needed
-		const filteredData = doheData
-			.filter( ( item ) => item.doha && item.doha.trim() !== "" ) // Filter out empty doha
+		// Process and filter data
+		const data = verses
+			.filter( ( item ) => item.verse_hi && item.verse_hi.trim() !== "" ) // Filter out empty dohas
 			.map( ( item ) => ( {
-				id: [ item.slug, item.index ].join( "-" ), // Assuming item has a slug property for ID
-				content: item.doha.split( "\n" ).map( ( doha ) => doha.trim() ),
-				author: SANT_KABIR, // Replace with actual author data if available
+				id: `${ item.slug }-${ padIndex( item.index ) }`, // Generate a unique ID using slug and index
+				author: SANT_KABIR, // Author of the dohas
+				content: item.verse_hi.split( "\n" ).map( ( verse ) => verse.trim() ), // Split and trim dohas
 			} ) );
 
-		const DohaMeta = {
+		const metaData = {
 			fileName: "santon-ke-dohe",
 			mdTitle: "संतों के दोहे (Couplets)",
-			data: filteredData, // Using filteredData instead of dataSet
+			data,
 		};
 
-		return DohaMeta;
+		return metaData;
 	} catch ( error ) {
 		console.error( "Error fetching data:", error );
-		throw error; // Optionally rethrow or handle the error as needed
+		throw error; // Rethrow the error to be handled by the caller
 	}
 }
 
