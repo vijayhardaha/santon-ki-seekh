7f95cab1456e086646992447ebafde965da480d0
Vijay Hardaha <vijayhardaha@proton.me>
2026-08-13 02:29:43 +0530

    refactor(constants): remove Acharya Prashant quote support
    - Remove ACHARYA_PRASHANT constant
    - Keep SANT_KABIR and GURU_KABIR constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -34,11 +34,6 @@ export const SANT_KABIR = 'संत कबीर दास साहेब';
 export const GURU_KABIR = 'गुरु कबीर दास साहेब';
 
 /**
- * Author name used for Acharya Prashant quotes.
- */
-export const ACHARYA_PRASHANT = 'आचार्य प्रशांत';
-
-/**
  * Hindi (Devanagari) digits for converting Latin numbers to Hindi numerals.
  */
 export const HINDI_DIGITS = ['०', '१', '२', '३', '४', '५', '६', '७', '८', '९'];
